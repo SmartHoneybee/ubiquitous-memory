4b1bac5d215e701c39a75271e5b941edfa260446
Bee <SmartHoneybee@users.noreply.github.com>
2017-10-21 17:41:28 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -ex
+printf '@edge http://nl.alpinelinux.org/alpine/edge/main' >> /etc/apk/repositories
 apk update
 apk add libjpeg-turbo-utils
-apk add pngquant
+apk add pngquant@edge
 apk add gifsicle
 apk add optipng
