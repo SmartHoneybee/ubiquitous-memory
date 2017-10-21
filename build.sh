676dbe140d61368eafe9e54dd4f6b02a5cb94e39
Bee <SmartHoneybee@users.noreply.github.com>
2017-10-21 20:00:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,10 +12,12 @@ mkdir -p ~/go/src/github.com/mattermost
 cd ~/go/src/github.com/mattermost
 V=4.3.0
 wget "https://github.com/mattermost/mattermost-server/archive/v${V}.tar.gz"
-tar xvf "v${V}.tar.gz"
+tar xf "v${V}.tar.gz"
+mv "mattermost-server-${V}" mattermost-server
 rm "v${V}.tar.gz"
 wget "https://github.com/mattermost/mattermost-webapp/archive/v${V}.tar.gz"
-tar xvf "v${V}.tar.gz"
+tar xf "v${V}.tar.gz"
+mv "mattermost-webapp-${V}" mattermost-webapp
 rm "v${V}.tar.gz"
 # hack
 apk add libjpeg-turbo-utils
