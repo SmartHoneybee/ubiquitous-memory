c4e8c91b57ef60cb5a7698d127bf4b20729a1a84
Bee <SmartHoneybee@users.noreply.github.com>
2018-10-17 17:28:58 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -56,10 +56,10 @@ if [ "$(id -u)" -eq 0 ]; then # as root user
 		"$(go env GOROOT)/pkg/$(go env GOOS)_$(go env GOARCH)"
 	# switch to build user
 	su --login "${BUILD_USER_NAME}" "$(command -v env)" \
-		GOARCH="${GOARCH}" \
-		GOOS="${GOOS}" \
+		GOARCH="$(go env GOARCH)" \
+		GOOS="$(go env GOOS)" \
 		MATTERMOST_RELEASE="${MATTERMOST_RELEASE}" \
-		"${0}" "${@}"
+		"$(command -v sh)" "${0}" "${@}"
 	# salvage build artifacts
 	cp --verbose "${BUILD_USER_HOME}/mattermost-${MATTERMOST_RELEASE}-$(go env GOOS)-$(go env GOARCH).tar.gz*" .
 	exit 0
