7b573cffcd2c2fd56a49408744cd1dcb253ee2aa
parnic-sks <chris@straykitestudios.com>
2021-10-12 08:42:39 -0500

    Update for Mattermost v6

--- dependabot/dummy.go
+++ dependabot/dummy.go
@@ -1,3 +1,5 @@
 package main
-import _ "github.com/mattermost/mattermost-server/v5/model"
-func main() {/* dummy file */}
+
+import _ "github.com/mattermost/mattermost-server/v6/model"
+
+func main() { /* dummy file */ }
